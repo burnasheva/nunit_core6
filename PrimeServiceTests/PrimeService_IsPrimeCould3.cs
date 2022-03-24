b9eedd7539349739b6750eaa9dfc5df20cd7b519
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2022-03-24 18:09:01 +0100

    add several more tests

---
+++ PrimeServiceTests/PrimeService_IsPrimeCould3.cs
@@ -0,0 +1,41 @@
+using System;
+using NUnit.Framework;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    [TestFixture]
+    public class PrimeService_IsPrimeCould3
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeCould3()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Test]
+        public void ReturnFalseGivenValueOf3()
+        {
+            Console.WriteLine("some stdOut text");
+            Console.Error.WriteLine("some stdErr text");
+            TestContext.WriteLine("message from TestContext");
+            
+            var result = _primeService.IsPrime(3);
+
+            Assert.IsFalse(result, $"3 should not be prime");
+        }
+        
+        [Test]
+        public void ThisTestShouldFail()
+        {
+            Console.WriteLine("some stdOut new text");
+            Console.Error.WriteLine("some stdErr text");
+            TestContext.WriteLine("message from TestContext");
+            
+            var result = _primeService.IsPrime(10);
+
+            Assert.IsTrue(result, $"10 should not be prime");
+        }
+    }
+}
