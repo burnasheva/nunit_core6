09aee8a3688267fb1371822ab1f94c2736bfd90b
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2022-04-16 17:16:42 +0200

    add several test categories

--- PrimeServiceTests/CheckAssumeCase.cs
+++ PrimeServiceTests/CheckAssumeCase.cs
@@ -5,6 +5,7 @@ namespace Prime.UnitTests.Services
     public class CheckAssumeCase
     {
         [Test]
+        [Category("Simple")]
         public void TestAssumeFalse()
         {
             Assume.That(false);
